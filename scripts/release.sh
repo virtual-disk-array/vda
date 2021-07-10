0ae2a96823aa7326ffd7b00b03493c4392f7ef23
yupeng <yupeng0921@gmail.com>
2021-07-10 03:25:32 +0000

    create zip file per os/arch

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,13 +10,18 @@ if [ "$version" == "" ]; then
     exit 1
 fi
 
+if [ "$notes_file" == "" ]; then
+    echo "notes_file is required"
+    exit 1
+fi
+
 if [ ! -f $notes_file ]; then
     echo "release notes file does not exist"
     exit 1
 fi
 
 curr_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-# go to root dir
+# go to the root dir
 cd $curr_dir/..
 
 echo "check version"
@@ -39,9 +44,10 @@ git push --tags
 
 docker push virtualdiskarray/vdacsi
 
-folder_name="vda_$version"
-zip_name="$folder_name.zip"
-cp -r _out $folder_name
+os_and_arch="linux_amd64"
+folder_name="vda_${os_and_arch}_${version}"
+zip_name="${folder_name}.zip"
+cp -r _out/$os_and_arch $folder_name
 zip -r $zip_name $folder_name
 
 gh release create $version $zip_name --title "vda $version" --notes-file $notes_file --draft
